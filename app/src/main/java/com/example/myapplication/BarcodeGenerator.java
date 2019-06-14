25b21aa029d382e01eabd0feab2ae5942e43c3ee
VC <tommy8595>
2019-06-14 16:38:48 +0700

    add signout and more

--- app/src/main/java/com/example/myapplication/BarcodeGenerator.java
+++ app/src/main/java/com/example/myapplication/BarcodeGenerator.java
@@ -1,13 +1,39 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class BarcodeGenerator extends AppCompatActivity {
+
+    private Button btnsignout;
+    private FirebaseAuth mAuth1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.barcode_generator);
+
+        mAuth1=FirebaseAuth.getInstance();
+        btnsignout = (Button)findViewById(R.id.signout);
+
+        btnsignout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signout();
+            }
+        });
+    }
+
+    //function signout
+    private void signout()
+    {
+        mAuth1.signOut();
+        startActivities(new Intent[]{new Intent(BarcodeGenerator.this, MainActivity.class)});
     }
 }
+
--- app/src/main/java/com/example/myapplication/Login_Activity.java
+++ app/src/main/java/com/example/myapplication/Login_Activity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,6 +25,8 @@ public class Login_Activity extends AppCompatActivity  {
     private Button btntest1;//sign up
     private EditText email;
     private EditText password;
+    //check user if it alr logging
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +54,23 @@ public class Login_Activity extends AppCompatActivity  {
                 signup();
             }
         });
+
+        //AuthListener check if it login
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser()!=null)
+                {
+                    startActivities(new Intent[]{new Intent(Login_Activity.this, BarcodeGenerator.class)});
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
     }
 
     //function sign in
@@ -59,18 +79,24 @@ public class Login_Activity extends AppCompatActivity  {
         String e =email.getText().toString();
         String p =password.getText().toString();
 
-        mAuth.signInWithEmailAndPassword(e,p)
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful())
-                        {
-                            Toast.makeText(Login_Activity.this,"Success",Toast.LENGTH_SHORT).show();
+        if (TextUtils.isEmpty(e)||TextUtils.isEmpty(p))
+        {
+            Toast.makeText(this,"Shouldn't leave blank",Toast.LENGTH_SHORT).show();
+        }else
+            mAuth.signInWithEmailAndPassword(e,p)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful())
+                            {
+                                startActivities(new Intent[]{new Intent(Login_Activity.this, BarcodeGenerator.class)});
+                            }
+                            else
+                                Toast.makeText(Login_Activity.this,"Not Success",Toast.LENGTH_SHORT).show();
                         }
-                        else
-                            Toast.makeText(Login_Activity.this,"Not Success",Toast.LENGTH_SHORT).show();
-                    }
-                });
+                    });
+
+
     }
     //function sign up
     private void signup()
@@ -78,7 +104,11 @@ public class Login_Activity extends AppCompatActivity  {
         String e =email.getText().toString();
         String p =password.getText().toString();
 
-        mAuth.createUserWithEmailAndPassword(e,p)
+        if (TextUtils.isEmpty(e)||TextUtils.isEmpty(p))
+        {
+            Toast.makeText(this,"Shouldn't leave blank",Toast.LENGTH_SHORT).show();
+        }else
+            mAuth.createUserWithEmailAndPassword(e,p)
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
