903b26120e435543a3ee1c5b38d778f63fa46f5f
VC <tommy8595>
2019-06-14 13:09:59 +0700

    add sign up nov check mel account login hz nov

---
+++ app/src/main/java/com/example/myapplication/Login_Activity.java
@@ -0,0 +1,94 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Login_Activity extends AppCompatActivity  {
+
+    //declare auth
+    private FirebaseAuth mAuth;
+
+    //get data
+    private Button btntest;//sign in
+    private Button btntest1;//sign up
+    private EditText email;
+    private EditText password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        mAuth=FirebaseAuth.getInstance();
+        btntest = (Button)findViewById(R.id.btntestxml);
+        btntest1 = (Button)findViewById(R.id.btntest1xml);
+        email =(EditText)findViewById(R.id.emailxml);
+        password=(EditText)findViewById(R.id.passwordxml);
+
+        //sign in
+        btntest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signin();
+            }
+        });
+
+        //sign up
+        btntest1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signup();
+            }
+        });
+    }
+
+    //function sign in
+    private void signin()
+    {
+        String e =email.getText().toString();
+        String p =password.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(e,p)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful())
+                        {
+                            Toast.makeText(Login_Activity.this,"Success",Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                            Toast.makeText(Login_Activity.this,"Not Success",Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+    //function sign up
+    private void signup()
+    {
+        String e =email.getText().toString();
+        String p =password.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(e,p)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful())
+                        {
+                            Toast.makeText(Login_Activity.this,"Register Complete",Toast.LENGTH_SHORT).show();
+                        }
+                        else Toast.makeText(Login_Activity.this,"Register Not Complete",Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+}
