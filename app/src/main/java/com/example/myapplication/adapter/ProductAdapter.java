b8ccea3cb67c3c8bae4b92e90b147a9d25fd4ad8
Phoeun-Chhantana <chhan1997@gmail.com>
2019-07-06 21:14:46 +0700

    Add product detail

--- app/src/main/java/com/example/myapplication/adapter/ProductAdapter.java
+++ app/src/main/java/com/example/myapplication/adapter/ProductAdapter.java
@@ -1,19 +1,27 @@
 package com.example.myapplication.adapter;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.databinding.DataBindingUtil;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.myapplication.R;
 import com.example.myapplication.data.DataProduct;
+import com.example.myapplication.databinding.ProductDetailLayoutBinding;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductViewHolder> {
 
@@ -62,6 +70,10 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductV
         TextView mTvProductCate;
         TextView mTvProductPrice;
 
+        private int mValue = 1;
+        private AlertDialog mDialog = null;
+        ProductDetailLayoutBinding mBindingDetail = null;
+
         private ProductViewHolder(View itemView) {
             super(itemView);
             mImgCover = (ImageView)itemView.findViewById(R.id.ivProductCover);
@@ -76,14 +88,114 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductV
             mTvProductName.setText(product.getProName());
             mTvProductCate.setText(product.getProCate());
 
-            String _priceString = String.format("%.2f",product.getProPrice()) + " USD ";
+            String _priceString = String.format(Locale.getDefault(),"%.2f USD",product.getProPrice());
             mTvProductPrice.setText(_priceString);
         }
 
         @Override
         public void onClick(View view) {
-            Toast.makeText(view.getContext(),String.valueOf(products.get(getAdapterPosition()).getProName()),
-                    Toast.LENGTH_SHORT).show();
+            try{
+                AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+                mDialog= builder
+                        .setTitle("Detail")
+                        .setView(createProductDetailDialog())
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        }).create();
+
+                mDialog.show();
+            }catch (Exception e){
+                Log.d("Exception",e.getMessage());
+            }
+        }
+
+        private View createProductDetailDialog(){
+            final View detailView = LayoutInflater.from(this.itemView.getContext())
+                    .inflate(R.layout.product_detail_layout,null);
+
+            mBindingDetail = DataBindingUtil.bind(detailView);
+
+            try{
+                if(mBindingDetail != null){
+
+                    final int MAX = 99;
+                    final int MIN = 1;
+                    //set default
+                    mBindingDetail.tvValueOfQty.setText(itemView.getResources().getString(R.string.default_qty_value));
+
+                    mBindingDetail.btnDecrement.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            if(mValue == MIN){
+                                mBindingDetail.tvValueOfQty.setText(String.valueOf(MIN));
+                            }
+                            else{
+                                mValue -= 1;
+                                mBindingDetail.tvValueOfQty.setText(String.valueOf(mValue));
+                            }
+                        }
+                    });
+                    mBindingDetail.btnIncrement.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            if(mValue < MAX){
+                                mValue += 1;
+                                mBindingDetail.tvValueOfQty.setText(String.valueOf(mValue));
+                            }
+                        }
+                    });
+
+                    mBindingDetail.imgProduct.setImageResource(products.get(getAdapterPosition()).getProCover());
+
+                    mBindingDetail.tvProductName.setText(products.get(getAdapterPosition()).getProName());
+
+                    mBindingDetail.tvProductCate.setText(products.get(getAdapterPosition()).getProCate());
+
+                    mBindingDetail.tvProductPrice.setText(String.format(Locale.getDefault(),
+                            "$%.2f",products.get(getAdapterPosition()).getProPrice()));
+
+                    setUpLetterSpacing(new View[] {
+                            mBindingDetail.tvDescription,
+                            mBindingDetail.tvProductPrice,
+                            mBindingDetail.tvProductName,
+                            mBindingDetail.tvProductCate,
+                            mBindingDetail.btnAddToCart,
+                            mBindingDetail.btnOrderNow
+                    });
+
+                    mBindingDetail.btnAddToCart.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+
+                        }
+                    });
+
+                    mBindingDetail.btnOrderNow.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+
+                        }
+                    });
+                }
+            }catch (Exception e){
+                Log.d("Exception",e.getMessage());
+            }
+
+            return detailView;
+        }
+
+        private void setUpLetterSpacing(View[] views){
+            for(View view:views){
+                if(view instanceof TextView){
+                    ((TextView) view).setLetterSpacing(0.1F);
+                }
+                if(view instanceof Button){
+                    ((Button) view).setLetterSpacing(0.1F);
+                }
+            }
         }
     }
 }
